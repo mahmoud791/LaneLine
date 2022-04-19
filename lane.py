382a4f03608cf6ae9f3a3786847c59b20dfd0f48
Mahmod 3ashry <64699750+mahmoud791@users.noreply.github.com>
2022-04-19 23:04:23 +0200

    draw_lane

--- lane.py
+++ lane.py
@@ -55,6 +55,75 @@ def sharpen_img(img):
 
 
 
+
+# Compute linear image transformation img*s+m
+def lin_img(img,s=1.0,m=0.0):
+    img2=cv2.multiply(img, np.array([s]))
+    return cv2.add(img2, np.array([m]))
+
+# Change image contrast; s>1 - increase
+def contr_img(img, s=1.0):
+    m=127.0*(1.0-s)
+    return lin_img(img, s, m)
+
+
+
+
+
+
+
+
+def draw_lane(img, video=False): #Draw found lane line onto a normal image
+    if video:
+        img, left_fitx, right_fitx, ploty, left, right = get_lane_video(img)
+    else:
+        img, left_fitx, right_fitx, ploty = get_lane(img, True)
+    warp_zero = np.zeros((IMAGE_H,IMAGE_W)).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    newwarp = cv2.warpPerspective(color_warp, Minv, (img.shape[1], img.shape[0])) 
+    # Combine the result with the original image
+    result = cv2.addWeighted(img, 1.0, newwarp, 0.6, 0)
+    if video:
+        # Add text information on the video frame
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        text_pos = 'Pos of the car: '+str(np.round(lane_offset(left, right),2))+ ' m'
+        radius = np.round(lane_curv(left, right),2)
+        if radius >= MAX_RADIUS:
+            radius = 'Inf'
+        else:
+            radius = str(radius)
+        text_rad = 'Radius: '+radius+ ' m'
+        cv2.putText(result,text_pos,(10,25), font, 1,(255,255,255),2)
+        cv2.putText(result,text_rad,(10,75), font, 1,(255,255,255),2)
+        return(result)
+
+
+
+
+
+
+
+
+
+def init_params(ran):
+    global right_fit_p, left_fit_p, n_count, RANGE, MIN_POINTS
+    right_fit_p = np.zeros(POL_ORD+1)
+    left_fit_p = np.zeros(POL_ORD+1)
+    n_count = 0
+    RANGE = ran
+    MIN_POINTS = 25-15*ran
+    
+def process_image(image):
+    return draw_lane(image, True)
+
+
 # Process videos
 
 def main():
