425dacd70d462859b07bad451e5126cc359ce816
El-Tabakh <73995540+El-Tabakh@users.noreply.github.com>
2022-04-19 17:54:27 +0200

    Add Histogram Normlization

---
+++ thresholding.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+
+
+def eq_Hist(img): # Histogram normalization
+    img[:, :, 0] = cv2.equalizeHist(img[:, :, 0])
+    img[:, :, 1] = cv2.equalizeHist(img[:, :, 1])
+    img[:, :, 2] = cv2.equalizeHist(img[:, :, 2])
+    return img
