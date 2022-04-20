0b8a10df4434e51eeea1e0d5ed1e35e431a31ba3
El-Tabakh <73995540+El-Tabakh@users.noreply.github.com>
2022-04-20 03:19:00 +0200

    Add Sobel Functions

--- thresholding.py
+++ thresholding.py
@@ -8,3 +8,35 @@ def eq_Hist(img): # Histogram normalization
     img[:, :, 1] = cv2.equalizeHist(img[:, :, 1])
     img[:, :, 2] = cv2.equalizeHist(img[:, :, 2])
     return img
+
+
+# Sobel
+def sobel_img(img, thresh_min = 25, thresh_max = 255, sobel_kernel = 11):
+    sobelx = np.absolute(cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel))
+    sobely = np.absolute(cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel))
+    scaled_sobelx = np.uint16(255*sobelx/np.max(sobelx))
+    scaled_sobely = np.uint16(255*sobely/np.max(sobely))
+    sobel_sum = scaled_sobelx+0.2*scaled_sobely
+    scaled_sobel_sum = np.uint8(255*sobel_sum/np.max(sobel_sum))
+    sum_binary = np.zeros_like(scaled_sobel_sum)
+    sum_binary[(scaled_sobel_sum >= thresh_min) & (scaled_sobel_sum <= thresh_max)] = 1
+    return sum_binary
+
+# Solbel magnitude
+def sobel_mag_img(img, thresh_min = 25, thresh_max = 255, sobel_kernel = 11):
+    sobelx = np.absolute(cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel))
+    sobely = np.absolute(cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel))
+    gradmag = np.sqrt(sobelx**2 + sobely**2)
+    scaled_gradmag = np.uint8(255*gradmag/np.max(gradmag))
+    gradmag_binary = np.zeros_like(scaled_gradmag)
+    gradmag_binary[(scaled_gradmag >= thresh_min) & (scaled_gradmag <= thresh_max)] = 1
+    return gradmag_binary
+
+# Sobel direction
+def sobel_dir_img(img, thresh_min = 0.0, thresh_max = 1.5, sobel_kernel = 11):
+    sobelx = np.absolute(cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel))
+    sobely = np.absolute(cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel))
+    graddir = np.arctan2(sobely, sobelx)
+    graddir_binary =  np.zeros_like(graddir)
+    graddir_binary[(graddir >= thresh_min) & (graddir <= thresh_max)] = 1
+    return  graddir_binary
