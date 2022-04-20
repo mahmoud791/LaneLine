3792b85049197cd5e69d8a67af3dbdc1397ff25a
Mahmod 3ashry <64699750+mahmoud791@users.noreply.github.com>
2022-04-20 03:24:59 +0200

    Update lane.py

--- lane.py
+++ lane.py
@@ -1,4 +1,3 @@
-
 # Load nessesery modules and set up
 import cv2
 import numpy as np
@@ -44,7 +43,6 @@ Minv = []
 
 
 
-
 def undistort(img):
 	return cv2.undistort(img, mtx, dist, None, mtx)
 
@@ -62,7 +60,6 @@ def lin_img(img,s=1.0,m=0.0):
 def contr_img(img, s=1.0):
     m=127.0*(1.0-s)
     return lin_img(img, s, m)
-
 
 # Create perspective image transformation matrices
 def create_M():
@@ -82,8 +79,106 @@ def transform(img, M):
     return warped
 
 
+# Choose the best polynomial order to fit points (x,y)
+
+def best_pol_ord(x, y):
+    pol1 = np.polyfit(y,x,1)
+    pred1 = pol_calc(pol1, y)
+    mse1 = mean_squared_error(x, pred1)
+    if mse1 < DEV_POL:
+        return pol1, mse1
+    pol2 = np.polyfit(y,x,2)
+    pred2 = pol_calc(pol2, y)
+    mse2 = mean_squared_error(x, pred2)
+    if mse2 < DEV_POL or mse1/mse2 < MSE_DEV:
+            return pol2, mse2
+    else:
+        pol3 = np.polyfit(y,x,3)
+        pred3 = pol_calc(pol3, y)
+        mse3 = mean_squared_error(x, pred3)
+        if mse2/mse3 < MSE_DEV:
+            return pol2, mse2
+        else:
+            return pol3, mse3
+    
+# Smooth polinomial functions of different degrees   
+def smooth_dif_ord(pol_p, x, y, new_ord):
+    x_p = pol_calc(pol_p, y)
+    x_new = (x+x_p)/2.0
+    return np.polyfit(y, x_new, new_ord)
 
 
+# Create virtual sensors
+def find(img, left=True, p_ord=POL_ORD, pol = np.zeros(POL_ORD+1), max_n = 0):
+    x_pos = [] #lists of found points
+    y_pos = []
+    max_l = img.shape[0] #number of lines in the img
+    for i in range(max_l-int(max_l*RANGE)):
+        y = max_l-i #Line number
+        y_01 = y / float(max_l) #y in [0..1] scale
+        if abs(pol[-1]) > 0: #If it not a still image or the first video frame
+            if y_01 >= max_n + SPEED: # If we can use pol to find center of the virtual sensor from the previous frame 
+                cent = int(pol_calc(pol, y_01-SPEED))
+                if y == max_l:
+                    if left:
+                        cent = 605
+                    else:
+                        cent = 690
+            else: # Prolong the pol tangentially
+                k = pol_d(pol, max_n)
+                b = pol_calc(pol, max_n)-k*max_n
+                cent = int(k*y_01+b)
+            if cent > IMAGE_W-WINDOW_SIZE:
+                cent = IMAGE_W-WINDOW_SIZE
+            if cent < WINDOW_SIZE:
+                cent = WINDOW_SIZE
+        else: #If it is a still image
+            if len(x_pos) > 0: # If there are some points detected
+                cent = x_pos[-1] # Use the previous point as a senser center
+            else: #Initial guess on line position
+                if left:
+                    cent = 605
+                else:
+                    cent = 690
+        if left: #Subsample image
+            sens = 0.5*s_hls(img[max_l-1-i:max_l-i,cent-WINDOW_SIZE:cent+WINDOW_SIZE,:])            +img[max_l-1-i:max_l-i,cent-WINDOW_SIZE:cent+WINDOW_SIZE,2]
+        else:
+            sens = img[max_l-1-i:max_l-i,cent-WINDOW_SIZE:cent+WINDOW_SIZE,2] # Red channel only for right white line
+        if len(sens[0,:]) < WINDOW_SIZE: #If we out of the image
+                break
+        x_max = max(sens[0,:]) #Find maximal value on the sensor
+        sens_mean = np.mean(sens[0,:])
+        # Get threshold
+        if left:
+            loc_thres = thres_l_calc(sens_mean)
+            loc_dev = DEV
+        else:
+            loc_thres = thres_r_calc(sens_mean)
+            loc_dev = DEV
+        if len(x_pos) == 0:
+            loc_dev = WINDOW_SIZE
+        if (x_max-sens_mean) > loc_thres and (x_max>100 or left):
+            if left:
+                x = list(reversed(sens[0,:])).index(x_max)
+                x = cent+WINDOW_SIZE-x
+            else:
+                x = list(sens[0,:]).index(x_max)
+                x = cent-WINDOW_SIZE+x
+            if x-1 < 569.0*y_01 or x+1 > 569.0*y_01+711 : #if the sensor touchs black triangle
+                break # We are done
+            if abs(pol[-1]) < 1e-4:  # If there are no polynomial provided     
+                x_pos.append(x)
+                y_pos.append(y_01)
+            else:
+                if abs(x-cent) < loc_dev: # If the found point deviated from expected position not significantly
+                    x_pos.append(x)
+                    y_pos.append(y_01)
+    if len(x_pos) > 1:
+        return x_pos, y_pos
+    else:
+        return [0], [0.0]
+
+# Get lines on a still image
 
 
 
